2c76d75ae16ad8a20a0f76c7a4ef5b0965feb901
Sam Lui <hhu354@aucklanduni.ac.nz>
2023-09-27 14:00:11 +1300

    Revert "process making gun manager"
    This reverts commit bf8b7034fc5e831cbaf07e3ade207d34e9ea940c.

--- data/GunData.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name GunData
-
-# Define gun properties
-@export var gun_name: String = "Default Gun"
-@export var damage: int = 2
-@export var ammo_capacity: int = 30
-@export var reload_time: float = 2.0
--- scripts/weapons.gd
+++ scripts/weapons.gd
@@ -2,16 +2,16 @@ extends Node2D
 
 @onready var marker_2d = $Node2D/Sprite2D/Marker2D
 @onready var bulletScene = preload("res://scenes/Bullet.tscn")
-@export var gun_data: GunData
 @onready var hud = get_tree().get_root().get_node("Game/HUD")
+@export var damage: int = 2
+@export var max_ammo: int = 10
+@export var reload_time: float = 2.0
 
-var current_ammo: int
+
+var current_ammo: int = max_ammo
 var is_reloading: bool = false
 var mouse_pressed = false
 
-func _ready():
-	# Load properties from the GunData resource
-	current_ammo = gun_data.ammo_capacity
 
 func _unhandled_input(_event):
 	if Input.is_action_pressed('attack') and not is_reloading:
@@ -23,25 +23,26 @@ func _unhandled_input(_event):
 			current_ammo -= 1
 
 		mouse_pressed = false
-		hud.update_ammo(current_ammo, gun_data.ammo_capacity)
+		hud.update_ammo(current_ammo, max_ammo)
 
 	if Input.is_action_just_released('reload') and not is_reloading:
 		start_reloading()
 		hud.show_reloading()
 		
+		
 func shoot_bullet():
 	var bullet = bulletScene.instantiate() as Bullet
 	var root = get_tree().get_root() 
 	root.add_child(bullet)
-	bullet.hitbox_component.damage = gun_data.damage
+	bullet.hitbox_component.damage = damage
 	bullet.global_position = marker_2d.global_position
 	bullet.direction = (get_global_mouse_position() - global_position).normalized()
 	bullet.rotation = bullet.direction.angle()
 
 func start_reloading():
 	is_reloading = true
-	await get_tree().create_timer(gun_data.reload_time).timeout
-	current_ammo = gun_data.ammo_capacity
+	await get_tree().create_timer(reload_time).timeout
+	current_ammo = max_ammo
 	is_reloading = false
-	hud.update_ammo(current_ammo, gun_data.ammo_capacity)
+	hud.update_ammo(current_ammo, max_ammo)
 	hud.hide_reloading()
