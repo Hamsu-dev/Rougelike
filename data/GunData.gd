bf8b7034fc5e831cbaf07e3ade207d34e9ea940c
Sam Lui <hhu354@aucklanduni.ac.nz>
2023-09-27 13:32:35 +1300

    process making gun manager

---
+++ data/GunData.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name GunData
+
+# Define gun properties
+@export var gun_name: String = "Default Gun"
+@export var damage: int = 2
+@export var ammo_capacity: int = 30
+@export var reload_time: float = 2.0
